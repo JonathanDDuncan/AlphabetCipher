d221e55977feb8941c4ac198744e63a632bc2ec4
Matthew Alexander <signsmatt@gmail.com>
2021-06-01 19:00:11 -0600

    Cryptify

---
+++ AlphabetCipher/Cipher.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabetCipher
+{
+    public class Cipher
+    {
+        public static string Alphabet()
+        {
+            return "abcdefghijklmnopqrstuvwxyz";
+        }
+
+        public static string AlphabetShift(int shiftBy)
+        {
+            return Alphabet().Substring(shiftBy) + Alphabet().Substring(0, shiftBy);
+        }
+
+        public static string Chart()
+        {
+            var count = 0;
+            var sb = new StringBuilder();
+
+            foreach (var item in Alphabet())
+            {
+                sb.AppendLine(AlphabetShift(count));
+                count++;
+            }
+
+            return sb.ToString();
+        }
+
+        public static string Replace(char row, char column)
+        {
+            var rowNum = (int)row - 97;
+            var columnNum = (int)column - 97;
+            var alphabetRow = AlphabetShift(rowNum);
+            var result = alphabetRow.Substring(columnNum, 1);
+
+            return result;
+        }
+
+        public static string Replace(string lookup, string text)
+        {
+            var count = 0;
+            var sb = new StringBuilder();
+
+            foreach (var row in lookup)
+            {
+                var col = text.Substring(count, 1).FirstOrDefault();
+                sb.Append(Replace(row, col));
+                count++;
+            }
+
+            return sb.ToString();
+        }
+
+        public static string Cryptify(string secret, string message)
+        {
+            var copies = (int) Math.Ceiling((double)message.Length / (double)secret.Length);
+            var fullSecret = new String(secret, copies);
+        }
+    }
+}
--- AlphabetCipherTest/UnitTest1.cs
+++ AlphabetCipherTest/UnitTest1.cs
@@ -1,3 +1,4 @@
+using AlphabetCipher;
 using System;
 using Xunit;
 
@@ -6,9 +7,104 @@ namespace AlphabetCipherTest
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void AlphabetTest()
         {
+            var result = Cipher.Alphabet();
+            var expected = "abcdefghijklmnopqrstuvwxyz";
+            Assert.Equal(expected, result);
+        }
 
+        [Fact]
+        public void AlphabetOffByOneTest()
+        {
+            var result = Cipher.AlphabetShift(1);
+            var expected = "bcdefghijklmnopqrstuvwxyza";
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void AlphabetOffByTwelveTest()
+        {
+            var result = Cipher.AlphabetShift(12);
+            var expected = "mnopqrstuvwxyzabcdefghijkl";
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void AlphabetChartTest()
+        {
+            var result = Cipher.Chart();
+            var expected = @"abcdefghijklmnopqrstuvwxyz
+bcdefghijklmnopqrstuvwxyza
+cdefghijklmnopqrstuvwxyzab
+defghijklmnopqrstuvwxyzabc
+efghijklmnopqrstuvwxyzabcd
+fghijklmnopqrstuvwxyzabcde
+ghijklmnopqrstuvwxyzabcdef
+hijklmnopqrstuvwxyzabcdefg
+ijklmnopqrstuvwxyzabcdefgh
+jklmnopqrstuvwxyzabcdefghi
+klmnopqrstuvwxyzabcdefghij
+lmnopqrstuvwxyzabcdefghijk
+mnopqrstuvwxyzabcdefghijkl
+nopqrstuvwxyzabcdefghijklm
+opqrstuvwxyzabcdefghijklmn
+pqrstuvwxyzabcdefghijklmno
+qrstuvwxyzabcdefghijklmnop
+rstuvwxyzabcdefghijklmnopq
+stuvwxyzabcdefghijklmnopqr
+tuvwxyzabcdefghijklmnopqrs
+uvwxyzabcdefghijklmnopqrst
+vwxyzabcdefghijklmnopqrstu
+wxyzabcdefghijklmnopqrstuv
+xyzabcdefghijklmnopqrstuvw
+yzabcdefghijklmnopqrstuvwx
+zabcdefghijklmnopqrstuvwxy
+";
+            Assert.Equal(expected, result);
+        }
+        [Fact]
+        public void ReplaceAETest()
+        {
+            var result = Cipher.Replace('a', 'e');
+            var expected = "e";
+            Assert.Equal(expected, result);
+        }
+        
+        [Fact]
+        public void ReplaceTWTest()
+        {
+            var result = Cipher.Replace('t', 'w');
+            var expected = "p";
+            Assert.Equal(expected, result);
+        }
+        
+        [Fact]
+        public void ReplaceAETWTest()
+        {
+            var result = Cipher.Replace("at", "ew");
+            var expected = "ep";
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ReplaceMessageTest()
+        {
+            var message = "meetmebythetree";
+            var secret = "sconessconessco";
+            var result = Cipher.Replace(secret, message);
+            var expected = "egsgqwtahuiljgs";
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void CryptifyTest()
+        {
+            var message = "meetmebythetree";
+            var secret = "scones";
+            var result = Cipher.Cryptify(secret, message);
+            var expected = "egsgqwtahuiljgs";
+            Assert.Equal(expected, result);
         }
     }
 }
